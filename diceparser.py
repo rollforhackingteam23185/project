ca9243f9bca3a2e71d0ec8ff90dcef1e775141a7
M. Gardner <magardner2010@gmail.com>
2020-08-07 23:44:07 +0300

    add prototype function

--- diceparser.py
+++ diceparser.py
@@ -1,2 +1,9 @@
 #step one: produce an easily computable format (i.e. RPN)
 
+def parse(string):
+    #...
+    # sample 1d20;
+    ret = []
+    for i in range(1,20):
+        ret[i]++;
+    return ret
