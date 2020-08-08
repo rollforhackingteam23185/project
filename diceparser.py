e678c74c356ea068b8e1a62dd8e89f2b21208c48
M. Gardner <magardner2010@gmail.com>
2020-08-08 18:08:05 +0300

    make it actually a 1d20

--- diceparser.py
+++ diceparser.py
@@ -1,10 +1,12 @@
 #step one: produce an easily computable format (i.e. RPN)
 
+def d():
+
 def parse(string):
     #...
     # sample 1d20;
     ret = {}
-    for i in range(1,20):
+    for i in range(1,21): # range is not inclusive at both ends??
         print i
         if i in ret:
             ret[i]=ret[i]+1
