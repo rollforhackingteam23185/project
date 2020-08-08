0009266ed473647fdd8b04820b2fd93036576a3c
M. Gardner <magardner2010@gmail.com>
2020-08-09 00:09:14 +0300

    calculate EXCEPT IT DOESN'T WORK!!!

--- diceparser.py
+++ diceparser.py
@@ -27,11 +27,82 @@ def parse(string):
                 result.append(stack[-1])
                 stack.pop()
             stack.pop()
-    result.append(stack[-1])
-    stack.pop()
+    while len(stack)!=0:
+        result.append(stack[-1])
+        stack.pop()
+    queue=[]
+    queue.append(result);
+    calculate = True;
+
+    while calculate:
+        calculate=False
+        stack=[]
+        queue2=[]
+        for q in queue:
+            if len(q) > 1:
+                calculate=True
+            else:
+                continue
+            i=0
+            while not q[i] in precedences:
+                i+=1
+                stack.append(q[i])
+            if q[i] == "d":
+                if(int(q[i-2]) != 1):
+                    r=q[:i-2]
+                    r.append("1")
+                    r.append(q[i-1])
+                    r.append("d")
+                    for _ in range(1,int(q[i-2])):
+                        r.append("1")
+                        r.append(q[i-1])
+                        r.append("d")
+                        r.append("+")
+                    r=r+q[i+1:]
+                    queue2.append(r)
+                    print(r)
+                else:
+                    r=q[:i-2]
+                    r.append(1)
+                    j=i-2
+                    r=r+q[i+1:]
+                    for r[i-2] in range(1,int(q[i-1])+1):
+                        queue2.append(r)
+                        print(r)
+            elif q[i] == "+":
+                r=q[:i-2]
+                r.append(str(int(q[i-2])+int(q[i-1])))
+                r=r+q[i+1:]
+                queue2.append(r)
+                print(r)
+            elif q[i] == "-":
+                r=q[:i-2]
+                r.append(str(int(q[i-2])-int(q[i-1])))
+                r=r+q[i+1:]
+                queue2.append(r)
+                print(r)
+            elif q[i] == "*":
+                r=q[:i-2]
+                r.append(str(int(q[i-2])*int(q[i-1])))
+                r=r+q[i+1:]
+                queue2.append(r)
+                print(r)
+            elif q[i] == "^":
+                r=q[:i-2]
+                r.append(str(int(q[i-2])**int(q[i-1])))
+                r=r+q[i+1:]
+                queue2.append(r)
+                print(r)
+
+
+        queue=queue2
+
+            
+
+
 
 def main():
-    print(parse("1 d 6"))
+    print(parse("2 d 6"))
 
 if __name__ == "__main__":
     main()
