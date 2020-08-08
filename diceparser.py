0a9987847ac2ab5d53a2976bbbd5fca1103e9717
M. Gardner <magardner2010@gmail.com>
2020-08-09 00:43:28 +0300

    remove debugging info and fix a few last bugs

--- diceparser.py
+++ diceparser.py
@@ -3,7 +3,7 @@
 def parse(string):
     #assuming all tokens seperated by whitespace
     tokens = string.split()
-    print(tokens)
+    #print(tokens)
     precedences = {"+": 10,"-":9, "*":7, "^":5, "d": 0}
     #turn into rpn
     stack = [];
@@ -13,7 +13,7 @@ def parse(string):
         if a.isdigit():
             result.append(a)
         elif a in precedences: #a is an operator
-            print(len(stack))
+            #print(len(stack))
             while len(stack)!=0 and (stack[-1] in precedences \
                     and (precedences[stack[-1]]<precedences[a] \
                     or (precedences[stack[-1]]==precedences[a] and a!="d"))):
@@ -33,7 +33,7 @@ def parse(string):
     queue=[]
     queue.append(result);
     calculate = True;
-
+    #print(queue)
     while calculate:
         calculate=False
         stack=[]
@@ -42,11 +42,11 @@ def parse(string):
             if len(q) > 1:
                 calculate=True
             else:
+                queue2.append(q.copy())
                 continue
             i=0
             while not q[i] in precedences:
                 i+=1
-                stack.append(q[i])
             if q[i] == "d":
                 if(int(q[i-2]) != 1):
                     r=q[:i-2]
@@ -60,43 +60,53 @@ def parse(string):
                         r.append("+")
                     r=r+q[i+1:]
                     queue2.append(r.copy())
-                    print(r)
+                    #print(r)
                 else:
                     r=q[:i-2]
                     r.append(1)
                     j=i-2
                     r=r+q[i+1:]
-                    for r[i-2] in range(1,int(q[i-1])+1):
+                    for j in range(1,int(q[i-1])+1):
+                        r[i-2]=str(j)
                         queue2.append(r.copy())
-                        print(r)
+                        #print(r)
             elif q[i] == "+":
                 r=q[:i-2]
                 r.append(str(int(q[i-2])+int(q[i-1])))
                 r=r+q[i+1:]
                 queue2.append(r.copy())
-                print(r)
+                #print(r)
             elif q[i] == "-":
                 r=q[:i-2]
                 r.append(str(int(q[i-2])-int(q[i-1])))
                 r=r+q[i+1:]
                 queue2.append(r.copy())
-                print(r)
+                #print(r)
             elif q[i] == "*":
                 r=q[:i-2]
                 r.append(str(int(q[i-2])*int(q[i-1])))
                 r=r+q[i+1:]
                 queue2.append(r.copy())
-                print(r)
+                #print(r)
             elif q[i] == "^":
                 r=q[:i-2]
                 r.append(str(int(q[i-2])**int(q[i-1])))
                 r=r+q[i+1:]
                 queue2.append(r.copy())
-                print(r)
-        queue=queue2
+                #print(r)
+        queue=queue2.copy()
+#        print(queue)
+    result={}
+    for q in queue:
+        for r in q:
+            if r in result:
+                result[r]+=1
+            else:
+                result[r]=1
+    return result
 
 def main():
-    print(parse("2 d 6"))
+    print(parse(" ( 1 d 6 ) d 6"))
 
 if __name__ == "__main__":
     main()
