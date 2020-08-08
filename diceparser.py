a742fcc022903bfaed15d43105606369eae8409c
M. Gardner <magardner2010@gmail.com>
2020-08-08 20:29:08 +0300

    add shunting-yard algorithm

--- diceparser.py
+++ diceparser.py
@@ -1,15 +1,37 @@
 #step one: produce an easily computable format (i.e. RPN)
 
-def d():
-
 def parse(string):
-    #...
-    # sample 1d20;
-    ret = {}
-    for i in range(1,21): # range is not inclusive at both ends??
-        print i
-        if i in ret:
-            ret[i]=ret[i]+1
-        else:
-            ret[i]=1
-    return ret
+    #assuming all tokens seperated by whitespace
+    tokens = string.split()
+    print(tokens)
+    precedences = {"+": 10,"-":9, "*":7, "^":5, "d": 0}
+    #turn into rpn
+    stack = [];
+    result = [];
+    for a in tokens:
+        a=a.strip()
+        if a.isdigit():
+            result.append(a)
+        elif a in precedences: #a is an operator
+            print(len(stack))
+            while len(stack)!=0 and stack[-1] in precedences \
+                    and precedences[stack[-1]]<precedences[a] \
+                    or precedences[stack[-1]]==precedences[a] and a!="d":
+                        result.append(stack[-1])
+                        stack.pop()
+            stack.append(a)
+        elif a=="(":
+            stack.append(a)
+        elif a==")":
+            while stack[-1]!="(":
+                result.append(stack[-1])
+                stack.pop()
+            stack.pop()
+    result.append(stack[-1])
+    stack.pop()
+
+def main():
+    print (parse("1 d 6"))
+
+if __name__ == "__main__":
+    main()
